876b6262f2a3195b6f9a6f88e03f1300667bc8ee
Ajay Khatri <amajaying@gmail.com>
2023-02-06 13:20:44 +0530

    fibonacci

--- Lab 04 - Feb 6/10.java
+++ Lab 04 - Feb 6/10.java
@@ -1,3 +1,22 @@
 //  Print the fibonacci series
 
 import java.util.*;
+
+class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("How many numbers to print?: ");
+        n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        System.out.print(a + " " + b + " ");
+        for(int i=0; i<n-2; i++){
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+};
