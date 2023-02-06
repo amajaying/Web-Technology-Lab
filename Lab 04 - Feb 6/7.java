07d8d796b2e0e44301b9859b489a37394f61a623
Ajay Khatri <amajaying@gmail.com>
2023-02-06 12:55:23 +0530

    all codes

--- Lab 04 - Feb 6/7.java
+++ Lab 04 - Feb 6/7.java
@@ -8,13 +8,25 @@ import java.util.*;
 class Solution{
     public static void main(String args[]){
         int n = 4;
+        int x = 0;
+        int one = 0;
         for(int i=0; i<n; i++){
-            for(int j=0; j<n-i; j++){
-                System.out.print(" ");
+            for(int j=0; j<n-i+1; j++){
+                if(i==0 && one == 0){
+                    System.out.print(" ");
+                    one = 1;
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
-            for(int k=0; k<=i+1; k++){
+            if(i==0){
+                System.out.print("*");
+            }
+            for(int k=0; k<x; k++){
                 System.out.print("* ");
             }
+            x=x+2;
             System.out.println();
         }
     }
