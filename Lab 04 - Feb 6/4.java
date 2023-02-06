ecd70d804cabc6a7d54c8488a354f9a0d6717d39
Ajay Khatri <amajaying@gmail.com>
2023-02-06 12:30:19 +0530

    code

---
+++ Lab 04 - Feb 6/4.java
@@ -0,0 +1,24 @@
+// check if user entered number is palindrome or not.
+
+import java.util.*;
+
+class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num, rev = 0, rem, temp;
+        System.out.print("Enter the number:"); 
+        num = sc.nextInt();
+        temp = num;
+        while(num > 0){
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+        }
+        if(temp == rev){
+            System.out.print("Palindrome");
+        }
+        else{
+            System.out.print("Not Palindrome");
+        }
+    }
+};
