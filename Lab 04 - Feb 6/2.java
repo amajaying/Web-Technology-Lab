22f822f1e641c124e2b7a01584d8d3c81b1b3aa5
Ajay Khatri <amajaying@gmail.com>
2023-02-06 12:22:48 +0530

    Java Day 1

---
+++ Lab 04 - Feb 6/2.java
@@ -0,0 +1,32 @@
+// Program to print the corresponding grade for the given mark using if else statement in Java
+
+import java.util.*;
+
+class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int mark = sc.nextInt();
+        System.out.print("Your grade is:");
+        if(mark >= 90){
+            System.out.print("O");
+        }
+        else if(mark >= 80){
+            System.out.print("E");
+        }
+        else if(mark >= 70){
+            System.out.print("A");
+        }
+        else if(mark >= 60){
+            System.out.print("B");
+        }
+        else if(mark>=50){
+            System.out.print("C");
+        }
+        else if(mark>=40){
+            System.out.print("D");
+        }
+        else{
+            System.out.print("Less than D");
+        }
+    }
+};
---
+++ Lab 04 - Feb 6/3.java
@@ -0,0 +1,8 @@
+
+import java.util.*;
+
+class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+    }
+};
