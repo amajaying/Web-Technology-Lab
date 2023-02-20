7b81be1b235eb2fc42189e4393246e930fe2c6a2
Ajay Khatri <amajaying@gmail.com>
2023-02-20 13:19:35 +0530

    codes

--- Lab 06 - Feb 20/2.java
+++ Lab 06 - Feb 20/2.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution{
     public static void main(String args[]){
         int even = 0, odd = 0;
-        for(int i = 0; i < args.length; i++){
+        for(int i = 0; i < 10; i++){
             if(Integer.parseInt(args[i]) % 2 == 0)
                 even++;
             else
