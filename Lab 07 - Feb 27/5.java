bce2ca2d4c8b1f3ee626b87f5f2dd323689e4e43
Ajay Khatri <amajaying@gmail.com>
2023-02-27 12:41:56 +0530

    code

---
+++ Lab 07 - Feb 27/5.java
@@ -0,0 +1,61 @@
+// Write a program to create an Account class containing acc_no, balance as datamembers and disp() to display the details. Inherit it in Person class with all mentioneddata members and functions. Person class also has name and aadhar_no as extra datamembers of its own. Override disp() function. Create 5 persons details
+
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args){
+        System.out.println("The details are:\n");
+        Person p1 = new Person();
+        p1.name = "Ajay";
+        p1.aadhar_no = 1;
+        p1.acc_no = 1001;
+        p1.balance = 10000;
+        p1.disp();
+        Person p2 = new Person();
+        p2.name = "Bishal";
+        p2.aadhar_no = 2;
+        p2.acc_no = 1002;
+        p2.balance = 20000;
+        p2.disp();
+        Person p3 = new Person();
+        p3.name = "Sampanna";
+        p3.aadhar_no = 3;
+        p3.acc_no = 1003;
+        p3.balance = 30000;
+        p3.disp();
+        Person p4 = new Person();
+        p4.name = "Dikshya";
+        p4.aadhar_no = 4;
+        p4.acc_no = 1004;
+        p4.balance = 40000;
+        p4.disp();
+        Person p5 = new Person();
+        p5.name = "Riya";
+        p5.aadhar_no = 5;
+        p5.acc_no = 1005;
+        p5.balance = 50000;
+        p5.disp();
+    } 
+};
+
+class Account{
+    int acc_no;
+    int balance;
+    void disp(){
+        System.out.println("Account Number: "+acc_no);
+        System.out.println("Balance: "+balance);
+    }
+};
+
+class Person extends Account{
+    String name;
+    int aadhar_no;
+    void disp(){
+        System.out.println("Name: "+name);
+        System.out.println("Aadhar Number: "+aadhar_no);
+        System.out.println("Account Number: "+acc_no);
+        System.out.println("Balance: "+balance);
+        System.out.println("---------------------");
+    }
+};
+
---
+++ Lab 07 - Feb 27/6.java
@@ -0,0 +1,46 @@
+// Write a program in java using inheritance to show how to call the base class parameterized constructors from the derived class using super.
+
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args){
+        C c = new C(1,2,3);
+        c.showA();
+        c.showB();
+        c.showC();
+    }
+};
+
+class A{
+    int aa;
+    A(int a){
+        a = a;
+    }
+    void showA(){
+        
+        System.out.println("A: "+aa);
+    }
+};
+
+class B extends A{
+    int bb;
+    B(int a,int b){
+        super(a);
+        bb = b;
+    }
+    void showB(){
+        System.out.println("B: "+bb);
+    }
+};
+
+class C extends B{
+    int cc;
+    C(int a,int b,int c){
+        super(a,b);
+        cc = c;
+    }
+    void showC(){
+        System.out.println("C: "+cc);
+    }
+};
+
