df0a29ca4b9f03000810f492f9d9a9a881c493f4
Ajay Khatri <amajaying@gmail.com>
2023-03-20 12:35:21 +0530

    code

---
+++ Lab 08 - Mar 20/q1.java
@@ -0,0 +1,55 @@
+// Write a program in Java to define a class Number with appropriate data members and member functions to input n number of integers and swap the biggestand smallest elements. Use member functions read(), swap() and display()
+
+import java.util.*;
+
+class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of integers: ");
+        int n = sc.nextInt();
+        Number obj = new Number(n);
+        obj.read();
+        obj.swap();
+        obj.display();
+    };
+};
+
+
+class Number{
+    int n;
+    int arr[];
+    Number(int num){
+        n = num;
+        arr = new int[n];
+    }
+    void read(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+    }
+    void swap(){
+        int max = arr[0], min = arr[0], maxIndex = 0, minIndex = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                maxIndex = i;
+            }
+            if(arr[i] < min){
+                min = arr[i];
+                minIndex = i;
+            }
+        }
+        int temp = arr[maxIndex];
+        arr[maxIndex] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    void display(){
+        System.out.println("The numbers after swapping are: ");
+        for(int i = 0; i < n; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+
